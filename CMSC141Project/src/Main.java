56efe96fc8d4c02fe60a8653ba98d492af970d71
Jay Vince Serato <jdserato@up.edu.ph>
2018-06-01 09:52:00 +0800

    Update Main.java

--- CMSC141Project/src/Main.java
+++ CMSC141Project/src/Main.java
@@ -3,23 +3,62 @@ import java.util.Scanner;
 
 public class Main {
     public static void main (String[] args){
-        System.out.println("Turing Machine Menu\n\t1. Power\n\t2. Even b's\n\nEnter 1 to to check if string is a^n.\nEnter 2 to check of string has even b's.");
         Scanner sc = new Scanner(System.in);
         int generate;
         do{
-            generate = sc.nextInt();
+            System.out.println("Turing Machine Menu\n\t1. Power\n\t2. Even B's\n\nEnter 1 to to check if string is x^y.\nEnter 2 to check of string has even b's.");
+            try {
+                generate = sc.nextInt();
+            } catch (InputMismatchException e) {
+                sc.nextLine();
+                generate = 0;
+            }
         }while(!(generate == 1 || generate == 2));
-        String string = "";
+        String string;
+        boolean error;
         switch(generate){
             case 1:
-                System.out.println("Enter string. (alphabet = {a,b})");
-                string = sc.next();
-                Power pow = new Power();
-                pow.check(string);
+                int x = 0, y = 0;
+                do {
+                    error = false;
+                    System.out.print("Enter number x (x^y): ");
+                    try {
+                        x = sc.nextInt();
+                        if (x <= 0) {
+                            error = true;
+                        }
+                    } catch (InputMismatchException e) {
+                        error = true;
+                        sc.nextLine();
+                    }
+                } while (error);
+                do {
+                    error = false;
+                    System.out.print("Enter number y (x^y): ");
+                    try {
+                        y = sc.nextInt();
+                        if (y <= 0) {
+                            error = true;
+                        }
+                    } catch (InputMismatchException e) {
+                        error = true;
+                        sc.nextLine();
+                    }
+                } while (error);
+                Power pow = new Power(x, y);
+                pow.check();
                 break;
             case 2:
-                System.out.println("Enter string. (alphabet = {a,b})");
-                string = sc.next();
+                do {
+                    error = false;
+                    System.out.println("Enter string. (alphabet = {a,b})");
+                    string = sc.next();
+                    for (char c : string.toCharArray()) {
+                        if (c != 'a' && c != 'b') {
+                            error = true;
+                        }
+                    }
+                } while (error);
                 Evenbs pal= new Evenbs();
                 pal.check(string);
                 break;
